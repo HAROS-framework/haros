e2c6ff28b005faf92bd7e86b8b56fd36699b7abd
git-afsantos <contact.andre.santos@gmail.com>
2021-12-22 10:36:06 +0000

    WIP metamodel classes and tests

--- src/haros/metamodel/ros.py
+++ src/haros/metamodel/ros.py
@@ -11,6 +11,8 @@ import enum
 
 import attr
 
+from haros.metamodel.common import SourceCodeMetadata, StorageMetadata
+
 ###############################################################################
 # Constants
 ###############################################################################
@@ -18,6 +20,7 @@ import attr
 
 @enum.unique
 class Languages(enum.Enum):
+    TEXT = 'Text'
     XML = 'XML'
     YAML = 'YAML'
     JSON = 'JSON'
@@ -36,8 +39,34 @@ class Languages(enum.Enum):
 ###############################################################################
 
 
-@attr.s(auto_attribs=True, slots=True)
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class File:
+    package: str
+    path: str  # relative path within package (e.g. 'src/code.cpp')
+    storage: StorageMetadata = attr.Factory(StorageMetadata)
+    source: SourceCodeMetadata = attr.Factory(SourceCodeMetadata)
+
+    @property
+    def uid(self) -> str:
+        return f'file:{self.package}/{self.path}'
+
+    @property
+    def name(self) -> str:
+        return self.path.rsplit(sep='/', maxsplit=1)[-1]
+
+    @property
+    def directory(self) -> str:
+        parts = self.path.rsplit(sep='/', maxsplit=1)
+        if len(parts) > 1:
+            return parts[0]
+        return '.'
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
 class RosPackage:
-    uid: str
     name: str
     files: typing.List[str] = attr.Factory(list)
+
+    @property
+    def uid(self) -> str:
+        return f'pkg:{self.name}'
