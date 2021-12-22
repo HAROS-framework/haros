6aab111de7095c0b354ca8b9a06dc927466a1754
git-afsantos <contact.andre.santos@gmail.com>
2021-12-22 11:43:10 +0000

    added more attributes to packages

--- src/haros/metamodel/common.py
+++ src/haros/metamodel/common.py
@@ -31,3 +31,15 @@ class SourceCodeMetadata:
     language: str = 'Text'  # e.g. C++, Python...
     lines: int = 1  # always at least 1, even if the file is empty
     ast: typing.Any = None
+
+
+@attr.s(auto_attribs=True, slots=True)
+class DevelopmentMetadata:
+    description: str = ''
+    authors: typing.Set[str] = attr.Factory(set)
+    maintainers: typing.Set[str] = attr.Factory(set)
+    version: str = 'unknown'
+    license: str = 'unknown'
+    url_home: typing.Optional[str] = None
+    url_source: typing.Optional[str] = None
+    url_tracker: typing.Optional[str] = None
