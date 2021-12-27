42c6898882caa4f3e9ce8ee810f14a18d5bc83f6
git-afsantos <contact.andre.santos@gmail.com>
2021-12-27 15:40:44 +0000

    WIP ROS entities

--- src/haros/metamodel/ros.py
+++ src/haros/metamodel/ros.py
@@ -38,6 +38,89 @@ class Languages(enum.Enum):
 
 
 ###############################################################################
+# ROS Source Entities
+###############################################################################
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class RosType:
+    # Parameters
+    package: str = attr.ib(validator=attr.validators.matches_re(RE_NAME))
+    name: str = attr.ib(validator=attr.validators.matches_re(RE_NAME))
+
+    def __str__(self) -> str:
+        return f'{self.package}/{self.name}'
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class RosName:
+    # Parameters
+    text: str
+
+    @property
+    def name(self) -> str:
+        name: str = self.text.rsplit(sep='/', maxsplit=1)[-1]
+        if name.startswith('~'):
+            return name[1:]
+        return name
+
+    @property
+    def namespace(self) -> str:
+        parts: List[str] = self.text.rsplit(sep='/', maxsplit=1)
+        if len(parts) > 1:
+            # e.g. 'a/b', '~a/b' or '/a'
+            return parts[0] if parts[0] else '/'
+        # else: e.g. 'a' or '~a'
+        return '~' if parts[0].startswith('~') else ''
+
+    @property
+    def is_global(self) -> bool:
+        return self.text.startswith('/')
+
+    @property
+    def is_private(self) -> bool:
+        return self.text.startswith('~')
+
+    @property
+    def is_relative(self) -> bool:
+        return not self.is_global and not self.is_private
+
+    def join(self, other: RosName) -> RosName:
+        """Uses `self` as a namespace to join with `other`."""
+        if other.is_global:
+            return other
+        suffix: str = other.text
+        if other.is_private:
+            assert suffix.startswith('~')
+            suffix = suffix[1:]
+        else:
+            assert other.is_relative
+        prefix: str = self.text
+        if prefix.endswith('/'):
+            prefix = prefix[:-1]
+        name: str = f'{prefix}/{suffix}'
+        return RosName(name)
+
+    def resolve(self, ns: RosName, private_ns: RosName) -> RosName:
+        """Resolves `self` relative to `ns` or `private_ns`."""
+        if self.is_global:
+            return self
+        if self.is_private:
+            return private_ns.join(self)
+        return ns.join(self)
+
+    def __str__(self) -> str:
+        return self.text
+
+
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class RosAdvertiseCall:
+    # Parameters
+    name: str
+    namespace: str
+
+
+###############################################################################
 # File System Objects
 ###############################################################################
 
@@ -89,11 +172,6 @@ class Package:
 
     def asdict(self) -> Dict[str, Any]:
         return attr.asdict(self)
-
-
-###############################################################################
-# ROS Source Objects
-###############################################################################
 
 
 @attr.s(auto_attribs=True, slots=True, frozen=True)
