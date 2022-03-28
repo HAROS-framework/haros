4c7bc24702e479fe9a5e4d4ad307eb209b3c3df4
git-afsantos <contact.andre.santos@gmail.com>
2022-03-28 12:00:49 +0100

    more CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,3 +19,21 @@ def test_zero_arguments():
         cli.main([])
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
+
+
+def test_invalid_cmd():
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        cli.main(['dummy', 'x', 'y', 'z'])
+    assert pytest_wrapped_e.type == SystemExit
+    assert pytest_wrapped_e.value.code == 2
+
+
+def test_echo_args():
+    cli.main(['echo-args', 'x', 'y', 'z'])
+
+
+def test_parse_echo_args():
+    args = cli.parse_arguments(['echo-args', 'x', 'y', 'z'])
+    assert isinstance(args, dict)
+    assert args.get('cmd') == 'echo-args'
+    assert args.get('args') == ['x', 'y', 'z']
