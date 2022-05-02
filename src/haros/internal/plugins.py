12f5d848addfc620b36fc991fcbc8b3a28249796
git-afsantos <contact.andre.santos@gmail.com>
2022-05-02 16:12:20 +0100

    added plugin interface

--- src/haros/internal/plugins.py
+++ src/haros/internal/plugins.py
@@ -5,10 +5,12 @@
 # Imports
 ###############################################################################
 
-from typing import Any, Dict, Final, List
+from typing import Any, Callable, Dict, Final, List
 
 from importlib import metadata
 import logging
+
+import attr
 
 ###############################################################################
 # Constants
@@ -18,12 +20,39 @@ ENTRY_POINT: Final[str] = 'haros.plugins'
 
 logger: Final[logging.Logger] = logging.getLogger(__name__)
 
+
+def _noop(*args, **kwargs):
+    pass
+
+
 ###############################################################################
 # Interface
 ###############################################################################
 
 
-def load(settings: Dict[str, Any]) -> List[Any]:
+@attr.s(auto_attribs=True, slots=True, frozen=True)
+class HarosPluginInterface:
+    # Attributes
+    name: str
+    module: Any
+    # Hooks
+    setup: Callable = _noop
+    on_analysis_begin: Callable = _noop
+    on_analysis_end: Callable = _noop
+
+
+    @classmethod
+    def from_module(cls, name, module):
+        return cls(
+            name,
+            module,
+            setup=getattr(module, 'haros_setup', _noop),
+            on_analysis_begin=getattr(module, 'haros_on_analysis_begin', _noop),
+            on_analysis_end=getattr(module, 'haros_on_analysis_end', _noop),
+        )
+
+
+def load(settings: Dict[str, Any]) -> List[HarosPluginInterface]:
     logger.info(f'plugins: searching {ENTRY_POINT}')
     plugins = []
     try:
@@ -31,10 +60,10 @@ def load(settings: Dict[str, Any]) -> List[Any]:
         for ep in eps:
             logger.info(f'plugins: found {ep.name} ({ep.value})')
             try:
-                plugin = ep.load()
+                module = ep.load()
                 logger.info(f'plugins: imported {ep.name}')
-                setup = getattr(plugin, 'haros_setup', _noop)
-                setup(**settings.get(ep.name, {}))
+                plugin = HarosPluginInterface.from_module(ep.name, module)
+                plugin.setup(**settings.get(ep.name, {}))
                 plugins.append(plugin)
                 logger.info(f'plugins: loaded {ep.name}')
             except Exception as e:
@@ -42,12 +71,3 @@ def load(settings: Dict[str, Any]) -> List[Any]:
     except KeyError:
         logger.warning('plugins: none to load')
     return plugins
-
-
-###############################################################################
-# Helper Functions
-###############################################################################
-
-
-def _noop(*args, **kwargs):
-    pass
