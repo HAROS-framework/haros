93cd6a1ad829d2c25e974d979b63fcdeddf83637
git-afsantos <contact.andre.santos@gmail.com>
2022-05-04 11:58:03 +0100

    using storage class

--- src/haros/internal/cli/analysis.py
+++ src/haros/internal/cli/analysis.py
@@ -16,7 +16,7 @@ import logging
 from pathlib import Path
 import re
 
-from haros.internal.fsutil import crawl_workspace, is_ros_package, is_workspace
+from haros.internal.fsutil import crawl_workspace, is_ros_package, is_workspace, StorageManager
 from haros.internal.plugins import load as load_plugins
 from haros.internal.settings import Settings
 
@@ -54,13 +54,13 @@ def run(args: Dict[str, Any], settings: Settings) -> int:
     if args['packages']:
         logger.error('analysis: discovery mode not yet supported')
         return 1
-    packages = process_paths(paths)
-    if not packages:
+    storage = process_paths(paths)
+    if not storage.packages:
         logger.error('analysis: did not find any ROS packages')
         return 1
-    logger.info(f'analysis: packages: {packages}')
+    logger.info(f'analysis: packages: {storage.packages}')
     plugins.on_analysis_begin()
-    print(f'analysis: packages: {list(packages.keys())}')
+    print(f'analysis: packages: {list(storage.packages.keys())}')
     plugins.on_analysis_end()
     return 0
 
@@ -105,22 +105,20 @@ def parse_arguments(argv: List[str]) -> Dict[str, Any]:
 ###############################################################################
 
 
-def process_paths(paths: List[Path]) -> Dict[str, Path]:
-    workspaces = []
-    packages = []
+def process_paths(paths: List[Path]) -> StorageManager:
+    storage = StorageManager()
+    adhoc = []
     for path in paths:
         if is_ros_package(path):
-            packages.append(path)
+            adhoc.append(path)
         else:
-            workspaces.append(path)
+            storage.workspaces.append(path)
             if not is_workspace(path):
                 logger.warning(f'analysis: workspace without "src" directory: {str(path)}')
-    index = {}
-    for ws in workspaces:
-        index.update(crawl_workspace(ws))
-    for path in packages:
-        index[path.name] = path
-    return index
+    storage.crawl()
+    for path in adhoc:
+        storage.packages[path.name] = path
+    return storage
 
 
 ###############################################################################
