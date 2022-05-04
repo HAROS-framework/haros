c084166c50deeab69221acdc2052a24f9c3fb294
git-afsantos <contact.andre.santos@gmail.com>
2022-05-04 10:34:35 +0100

    minor changes

--- src/haros/metamodel/common.py
+++ src/haros/metamodel/common.py
@@ -5,7 +5,7 @@
 # Imports
 ###############################################################################
 
-import typing
+from typing import Any, Optional, Set
 
 import attr
 
@@ -16,8 +16,8 @@ import attr
 
 @attr.s(auto_attribs=True, slots=True)
 class StorageMetadata:
-    path: typing.Optional[str] = None  # real path (e.g. '/home/user/file')
-    size: typing.Optional[int] = None  # in bytes
+    path: Optional[str] = None  # real path (e.g. '/home/user/file')
+    size: Optional[int] = None  # in bytes
     timestamp: int = 0
 
 
@@ -30,22 +30,22 @@ class StorageMetadata:
 class SourceCodeMetadata:
     language: str = 'Text'  # e.g. C++, Python...
     lines: int = 1  # always at least 1, even if the file is empty
-    ast: typing.Any = None
+    ast: Any = None
 
 
 @attr.s(auto_attribs=True, slots=True)
 class DevelopmentMetadata:
     description: str = ''
-    authors: typing.Set[str] = attr.Factory(set)
-    maintainers: typing.Set[str] = attr.Factory(set)
+    authors: Set[str] = attr.Factory(set)
+    maintainers: Set[str] = attr.Factory(set)
     version: str = 'unknown'
     license: str = 'unknown'
-    url_home: typing.Optional[str] = None
-    url_source: typing.Optional[str] = None
-    url_tracker: typing.Optional[str] = None
+    url_home: Optional[str] = None
+    url_source: Optional[str] = None
+    url_tracker: Optional[str] = None
 
 
 @attr.s(auto_attribs=True, slots=True, frozen=True)
 class SourceCodeDependencies:
-    build: typing.Set[str] = attr.Factory(set)
-    runtime: typing.Set[str] = attr.Factory(set)
+    build: Set[str] = attr.Factory(set)
+    runtime: Set[str] = attr.Factory(set)
