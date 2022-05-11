264450f5226d979f082c1260008518f07c07a00d
git-afsantos <contact.andre.santos@gmail.com>
2022-05-11 18:21:07 +0100

    fixed error

--- tests/ws1/src2/cmake/problem.cmake
+++ tests/ws1/src2/cmake/problem.cmake
@@ -1,9 +1,19 @@
-add_custom_command(
-    OUTPUT "Mempool.hpp"
-    COMMAND CycloneDDS::idlc
-    ARGS -l $<TARGET_FILE:CycloneDDS-CXX::idlcxx> ${MEMPOOL_IDL}
-    DEPENDS CycloneDDS::idlc CycloneDDS-CXX::idlcxx
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    COMMENT "Generating serialization for Mempool bytestream"
-    VERBATIM
-)
+if(MAKE_UNIQUE_INCLUDEDIR)
+  set(_unique_dir "iceoryx/v${IOX_VERSION_STRING}")
+  if(PREFIX STREQUAL "")
+    set(PREFIX "${_unique_dir}")
+  else()
+    set(PREFIX "${PREFIX}/${_unique_dir}")
+  endif()
+endif()
+
+if(USE_CYCLONE_DDS)
+    add_custom_command(
+        OUTPUT "Mempool.hpp"
+        COMMAND CycloneDDS::idlc
+        ARGS -l $<TARGET_FILE:CycloneDDS-CXX::idlcxx> ${MEMPOOL_IDL}
+        DEPENDS CycloneDDS::idlc CycloneDDS-CXX::idlcxx
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Generating serialization for Mempool bytestream"
+        VERBATIM
+    )
