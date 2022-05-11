5eefd29f464207de351c16fddab42d94bfde3216
git-afsantos <contact.andre.santos@gmail.com>
2022-05-11 15:17:04 +0100

    more tests

---
+++ tests/ws1/src2/cmake/comments.cmake
@@ -0,0 +1,23 @@
+#
+# LINE_COMMENT #1
+#LINE_COMMENT #2
+#[LINE_COMMENT #3]
+#[ [ LINE_COMMENT #4 ]]
+#[#[[ LINE_COMMENT #5 ]]]
+
+#[[]]
+#[[ BRACKET_COMMENT #1 ]]
+#[=[ BRACKET_COMMENT #2 ]] [[ ]=]
+
+#[[
+  BRACKET_COMMENT #3
+]]
+
+#[[ BRACKET_COMMENT
+  #4
+]]
+
+#[==[
+  BRACKET_COMMENT #5
+  #[[]]
+]==]
