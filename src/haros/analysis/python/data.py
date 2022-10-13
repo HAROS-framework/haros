649095d807ac775785dc827833f76b7c7a968f2f
git-afsantos <contact.andre.santos@gmail.com>
2022-10-13 15:37:08 +0100

    WIP data flow

--- src/haros/analysis/python/data.py
+++ src/haros/analysis/python/data.py
@@ -7,13 +7,14 @@
 
 from typing import Any, Final, List, Optional
 
-from enum import Enum
+import enum
 
 from attrs import define, field, frozen
 
 from haros.metamodel.common import VariantData
 from haros.metamodel.logic import TRUE, LogicValue
 from haros.parsing.python.ast import (
+    PythonAst,
     PythonClassDefStatement,
     PythonFunctionDefStatement,
     PythonImportStatement,
@@ -22,6 +23,8 @@ from haros.parsing.python.ast import (
 ###############################################################################
 # Constants
 ###############################################################################
+
+BUILTINS_MODULE: Final[str] = '__builtins__'
 
 BUILTIN_FUNCTIONS: Final[List[str]] = [
     'abs',
@@ -168,55 +171,140 @@ BUILTIN_EXCEPTIONS: Final[List[str]] = [
     'ResourceWarning',
 ]
 
+UNKNOWN_VALUE: Final[object] = object()
+
+
+class PythonType(enum.Flag):
+    BOOL = enum.auto()
+    INT = enum.auto()
+    FLOAT = enum.auto()
+    STRING = enum.auto()
+    FUNCTION = enum.auto()
+    CLASS = enum.auto()
+    EXCEPTION = enum.auto()
+    OBJECT = enum.auto()
+    ANY = BOOL | INT | FLOAT | STRING | FUNCTION | CLASS | EXCEPTION | OBJECT
+
+
 ###############################################################################
 # Data Structures
 ###############################################################################
 
 
-class PythonType(Enum):
-    BOOL = 'bool'
-    INT = 'int'
-    FLOAT = 'float'
-    STRING = 'string'
-    FUNCTION = 'function'
-    CLASS = 'class'
-    EXCEPTION = 'exception'
-    OBJECT = 'object'
+@frozen
+class AnnotatedValue:
+
+    def cast_to(self, value_type: PythonType) -> 'AnnotatedValue':
+        if self.type == value_type:
+            return self
+        new_type = self.type & value_type
+        if bool(new_type):
+            return AnnotatedValue(self.value, type=new_type)
+        raise TypeError(f'unable to cast {self.type} to {value_type}')
+
+
+_UNKNOWN_ANY: Final[AnnotatedValue] = AnnotatedValue.of(UNKNOWN_VALUE)
+_UNKNOWN_FUNCTION: Final[AnnotatedValue] = _UNKNOWN_ANY.cast_to(PythonType.FUNCTION)
+_UNKNOWN_CLASS: Final[AnnotatedValue] = _UNKNOWN_ANY.cast_to(PythonType.CLASS)
+_UNKNOWN_EXCEPTION: Final[AnnotatedValue] = _UNKNOWN_ANY.cast_to(PythonType.EXCEPTION)
 
 
 @frozen
 class Definition:
-    name: str
-    data: VariantData[Any]
+    value: Any
+    type: PythonType = PythonType.ANY
+    ast: Optional[PythonAst] = None
     import_base: str = ''
-    data_type: PythonType = PythonType.OBJECT
-    line: int = 0
-    column: int = 0
 
     @property
-    def is_import(self) -> bool:
+    def line(self) -> int:
+        return 0 if self.ast is None else getattr(self.ast, 'line', 0)
+
+    @property
+    def column(self) -> int:
+        return 0 if self.ast is None else getattr(self.ast, 'column', 0)
+
+    @property
+    def is_imported(self) -> bool:
         return bool(self.import_base)
+
+    @property
+    def is_builtin(self) -> bool:
+        return self.import_base == BUILTINS_MODULE
+
+    @property
+    def is_bool_type(self) -> bool:
+        return bool(self.type & PythonType.BOOL)
+
+    @property
+    def is_int_type(self) -> bool:
+        return bool(self.type & PythonType.INT)
+
+    @property
+    def is_float_type(self) -> bool:
+        return bool(self.type & PythonType.FLOAT)
+
+    @property
+    def is_number_type(self) -> bool:
+        return self.is_int or self.is_float
+
+    @property
+    def is_string_type(self) -> bool:
+        return bool(self.type & PythonType.STRING)
+
+    @property
+    def is_function_type(self) -> bool:
+        return bool(self.type & PythonType.FUNCTION)
+
+    @property
+    def is_class_type(self) -> bool:
+        return bool(self.type & PythonType.CLASS)
+
+    @property
+    def is_exception_type(self) -> bool:
+        return bool(self.type & PythonType.EXCEPTION)
+
+    @property
+    def is_object_type(self) -> bool:
+        return bool(self.type & PythonType.OBJECT)
 
     @classmethod
     def of_builtin_function(cls, name: str) -> 'Definition':
         value = getattr(__builtins__, name)
         assert callable(value),  f'expected function, got: {value!r}'
-        data = VariantData.with_base_value(value)
-        return cls(name, data, import_base='__builtins__', data_type=PythonType.FUNCTION)
+        return cls(value, type=PythonType.FUNCTION, import_base=BUILTINS_MODULE)
 
     @classmethod
     def of_builtin_class(cls, name: str) -> 'Definition':
         value = getattr(__builtins__, name)
         assert isinstance(value, type), f'expected class, got: {value!r}'
-        data = VariantData.with_base_value(value)
-        return cls(name, data, import_base='__builtins__', data_type=PythonType.CLASS)
+        return cls(value, type=PythonType.CLASS, import_base=BUILTINS_MODULE)
 
     @classmethod
     def of_builtin_exception(cls, name: str) -> 'Definition':
         value = getattr(__builtins__, name)
         assert isinstance(value, BaseException), f'expected exception, got: {value!r}'
-        data = VariantData.with_base_value(value)
-        return cls(name, data, import_base='__builtins__', data_type=PythonType.EXCEPTION)
+        return cls(value, type=PythonType.EXCEPTION, import_base=BUILTINS_MODULE)
+
+    @classmethod
+    def from_value(cls, value: Any, ast: Optional[PythonAst] = None) -> 'Definition':
+        if value is UNKNOWN_VALUE:
+            return cls(value, ast=ast)
+        if isinstance(value, bool):
+            return cls(value, type=PythonType.BOOL, ast=ast)
+        if isinstance(value, int):
+            return cls(value, type=PythonType.INT, ast=ast)
+        if isinstance(value, float):
+            return cls(value, type=PythonType.FLOAT, ast=ast)
+        if isinstance(value, str):
+            return cls(value, type=PythonType.STRING, ast=ast)
+        if isinstance(value, BaseException):
+            return cls(value, type=PythonType.EXCEPTION, ast=ast)
+        if isinstance(value, type):
+            return cls(value, type=PythonType.CLASS, ast=ast)
+        if callable(value):
+            return cls(value, type=PythonType.FUNCTION, ast=ast)
+        return cls(value, type=PythonType.OBJECT, ast=ast)
 
 
 ###############################################################################
@@ -226,48 +314,44 @@ class Definition:
 
 @define
 class DataScope:
-    defs: Dict[str, Definition] = field(factory=dict)
+    variables: Dict[str, VariantData[Definition]] = field(factory=dict)
     condition: LogicValue = TRUE
 
     @classmethod
     def with_builtins(cls) -> 'DataScope':
-        defs = {}
+        variables = {}
         for name in BUILTIN_FUNCTIONS:
-            defs[name] = Definition.of_builtin_function(name)
+            variables[name] = VariantData.with_base_value(Definition.of_builtin_function(name))
         for name in BUILTIN_CLASSES:
-            defs[name] = Definition.of_builtin_class(name)
+            variables[name] = VariantData.with_base_value(Definition.of_builtin_class(name))
         for name in BUILTIN_EXCEPTIONS:
-            defs[name] = Definition.of_builtin_exception(name)
-        return cls(defs=defs)
+            variables[name] = VariantData.with_base_value(Definition.of_builtin_exception(name))
+        return cls(variables=variables)
 
     def duplicate(self) -> 'DataScope':
-        defs = {k, v.duplicate() for k, v in self.defs.items()}
-        return self.__class__(defs=defs)
+        variables = {k: v.duplicate() for k, v in self.variables.items()}
+        return self.__class__(variables=variables)
 
-    def get(self, name: str) -> Optional[Definition]:
-        return self.defs.get(name)
+    def get(self, name: str) -> VariantData[Definition]:
+        return self.variables.get(name, VariantData())
 
     def set(
         self,
         name: str,
         value: Any,
+        type: PythonType = PythonType.ANY,
+        ast: Optional[PythonAst] = None,
         import_base: str = '',
-        data_type: PythonType = PythonType.OBJECT,
-        line: int = 0,
-        column: int = 0,
     ):
-        definition = self.defs.get(name)
-        if definition is None:
-            definition = Definition(
-                name,
-                VariantData(),
-                import_base=import_base,
-                data_type=data_type,
-                line=line,
-                column=column,
-            )
-            self.defs[name] = definition
-        definition.data.set(value, self.condition)
+        definition = Definition(value, type=type, ast=ast, import_base=import_base)
+        self.define(name, definition)
+
+    def define(self, name: str, definition: Definition):
+        var = self.variables.get(name)
+        if var is None:
+            var = VariantData()
+            self.variables[name] = var
+        var.set(definition, self.condition)
 
     def add_import(self, statement: PythonImportStatement):
         assert statement.is_statement and statement.is_import
@@ -275,27 +359,13 @@ class DataScope:
             if imported_name.is_wildcard:
                 continue  # FIXME
             name = imported_name.alias if imported_name.alias else imported_name.name
-            value = Definition(
-                name,
-                VariantData(),
-                import_base=imported_name.base.dotted_name,
-                line=imported_name.line,
-                column=imported_name.column,
-            )
-            self.set(value)
+            import_base = imported_name.base.dotted_name
+            self.set(name, UNKNOWN_VALUE, ast=statement, import_base=import_base)
 
     def add_function_def(self, statement: PythonFunctionDefStatement):
         assert statement.is_statement and statement.is_function_def
-        name = statement.name
-        data_type = PythonType.FUNCTION
-        line = statement.line
-        column = statement.column
-        self.set(name, statement, data_type=data_type, line=line, column=column)
+        self.set(statement.name, UNKNOWN_VALUE, type=PythonType.FUNCTION, ast=statement)
 
     def add_class_def(self, statement: PythonClassDefStatement):
         assert statement.is_statement and statement.is_class_def
-        name = statement.name
-        data_type = PythonType.CLASS
-        line = statement.line
-        column = statement.column
-        self.set(name, statement, data_type=data_type, line=line, column=column)
+        self.set(statement.name, UNKNOWN_VALUE, type=PythonType.CLASS, ast=statement)
