7d9b07bd42e821aea40234d86e0009b88f54f1b5
git-afsantos <contact.andre.santos@gmail.com>
2023-01-02 13:57:33 +0000

    tests

--- src/haros/analysis/launch/python.py
+++ src/haros/analysis/launch/python.py
@@ -27,6 +27,31 @@ LAUNCH_ENTRY_POINT: Final[str] = 'generate_launch_description'
 ###############################################################################
 
 
+def launch_description_function(arg_list):
+    # arg_list.is_resolved ?
+    return "launch.LaunchDescription"
+
+
+def declare_launch_argument_function(name, default_value=None, description=None):
+    arg = name.value if name.is_resolved else '?'
+    value = default_value.value if (default_value is not None and default_value.is_resolved) else '?'
+    text = description.value if (description is not None and description.is_resolved) else ''
+    return f'DeclareLaunchArgument({arg!r}, default_value={value!r}, description={text!r})'
+
+
+def launch_configuration_function(name, default=None):
+    cfg = name.value if name.is_resolved else '?'
+    value = default.value if (default is not None and default.is_resolved) else '?'
+    return f'LaunchConfiguration({cfg!r}, default={value!r})'
+
+
+LAUNCH_SYMBOLS = {
+    'launch.LaunchDescription': launch_description_function,
+    'launch.actions.DeclareLaunchArgument': declare_launch_argument_function,
+    'launch.substitutions.LaunchConfiguration': launch_configuration_function,
+}
+
+
 def get_python_launch_model(path: Path) -> LaunchModel:
     if not path.is_file():
         raise ValueError(f'not a file: {path}')
@@ -41,5 +66,6 @@ def get_python_launch_model(path: Path) -> LaunchModel:
         'mymodule.MY_CONSTANT': 44,
         'mymodule.my_division': lambda a, b: (a.value // b.value) if a.is_resolved and b.is_resolved else None,
     }
+    symbols.update(LAUNCH_SYMBOLS)
     graph = from_ast(ast, symbols=symbols)
     return graph
