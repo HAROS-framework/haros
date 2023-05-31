fd9e5a0f4d3c1d0ccbb82ddbd78b56925a786003
git-afsantos <contact.andre.santos@gmail.com>
2023-05-31 11:28:50 +0100

    boilerplate code

--- src/harosviz/client/js/main.js
+++ src/harosviz/client/js/main.js
@@ -1,2 +1,43 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2023 André Santos
+
+const { createApp } = Vue;
+
+const app = createApp({
+  data() {
+    return {
+      title: "",
+      text: "",
+      ui: {
+        state: 1
+      }
+    };
+  },
+
+  computed: {
+    isSetupState() {
+      return this.ui.state === 1; // UIState.SETUP
+    }
+  },
+
+  methods: {
+    onSetupDone() {
+    }
+
+    // refreshSlides() {
+    //   const slidesContainer = document.getElementById("slides-container");
+    //   const slide = document.querySelector(".slide");
+    //   const slideWidth = slide.clientWidth;
+    //   slidesContainer.scrollLeft = this.currentTurn * slideWidth;
+    // }
+  },
+
+  mounted() {
+    this.title = "Vue Application";
+    this.text = "Hello, world!";
+  }
+});
+
+app.component("SetupComponent", SetupComponent);
+
+app.mount("#app");
